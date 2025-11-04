b91ce78017b496a9790b5612fabde34a31639146
66017580-afk <66017580@rbac.ac.th>
2025-11-04 12:07:22 +0700

    commit3

--- aboutus.php
+++ aboutus.php
@@ -2,3 +2,11 @@
    echo "My name is aj yai";
    echo "I' m a teacher";
 ?>
+<?php
+   echo "My name is aj yai";
+   echo "I' m a teacher";
+?>
+<?php
+   echo "My name is aj yai";
+   echo "I' m a teacher";
+?>
